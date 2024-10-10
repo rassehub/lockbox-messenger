d699e3017aac8b6fff23a40fcc3070e375c59e14
rassehub <rasmus.mannik@gmail.com>
2024-10-11 02:17:34 +0300

    docker enviroment with hot reload in backend and frontend

---
+++ backend/src/main.rs
@@ -0,0 +1,16 @@
+use actix_web::{web, App, HttpServer, Responder};
+
+async fn index() -> impl Responder {
+    "juustoaaa"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index))
+    })
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await
+}
