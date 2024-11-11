09362d0ab774bda4d8b7218a7abf40624edb4ab4
mkytomies <moonakytomies@gmail.com>
2024-11-11 17:19:19 +0200

    Feat: Initial draft of home screen and its widgets

--- frontend/lib/screens/home/home_screen.dart
+++ frontend/lib/screens/home/home_screen.dart
@@ -1,9 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/widgets/home/chat_list.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              'Messages',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Container(
+              width: 450,
+              height: 40,
+              child: SearchBar(
+                hintText: 'Search',
+              ),
+            ),
+          ),
+          Expanded(
+            child: ChatList(),
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  shape: CircleBorder(),
+                  padding: EdgeInsets.all(20),
+                ),
+                child: Icon(Icons.add),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
