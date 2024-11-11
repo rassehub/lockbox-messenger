18689ee26d0bbb1260fdae27d2328ebbeb029153
mkytomies <moonakytomies@gmail.com>
2024-11-11 17:16:37 +0200

    Feat: First draft of navigation bar and routes

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
 import 'lockbox.dart';
+import 'package:frontend/screens/home/home_screen.dart';
+import 'package:frontend/screens/profile/profile_screen.dart';
+import 'package:frontend/widgets/common/nav_bar.dart';
 
 void main() {
-  runApp(Lockbox());
+  runApp(MaterialApp(
+    title: 'Lockbox',
+    initialRoute: '/main',
+    routes: {
+      '/main': (context) => MainLayout(),
+      '/home': (context) => HomeScreen(),
+      '/profile': (context) => ProfileScreen(),
+    },
+  ));
 }
