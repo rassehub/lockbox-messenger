62aea66a82814b33379f1d20fdb089f653d53e96
mkytomies <moonakytomies@gmail.com>
2025-04-01 17:01:23 +0300

    Empty frontend

---
+++ frontend/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  View,
+} from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+
+function App(): React.JSX.Element {
+  return (
+      <View>
+      
+      </View>
+  );
+}
+
+export default App;
