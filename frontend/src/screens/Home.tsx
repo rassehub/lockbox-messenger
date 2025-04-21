0174d68116db96efdccb4f5fa26efe7adee0428c
mkytomies <moonakytomies@gmail.com>
2025-04-21 12:31:44 +0300

    Add chat list and search bar to home screen

--- frontend/src/screens/Home.tsx
+++ frontend/src/screens/Home.tsx
@@ -1,16 +1,29 @@
 import { View, Text, StyleSheet } from "react-native";
 import React from "react";
+import SearchBar from "../components/SearchBar";
+import ChatList from "../components/ChatList";
 
 const HomeScreen = () => {
     return (
         <View style={styles.mainContainer}>
-            <Text>Messages</Text>
+            <Text style={styles.title}>Messages</Text>
+            <SearchBar />
+            <ChatList />
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     mainContainer: {
+        paddingHorizontal: '5%',
+        paddingTop: '25%',
+        paddingBottom: '5%',
+    },
+    title: {
+        fontSize: 32,
+        fontWeight: 'bold',
+        alignSelf: 'center',
+        color: '#594EFF',
     },
 })
 
