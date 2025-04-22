81b2e4cfa5011abb021c020d1a57111583eaaf61
mkytomies <moonakytomies@gmail.com>
2025-04-22 14:06:40 +0300

    Add modal

--- frontend/src/screens/NewChat.tsx
+++ frontend/src/screens/NewChat.tsx
@@ -1,15 +1,40 @@
-import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+import { Image, Modal, Pressable, StyleSheet, Text, View } from "react-native";
 import SearchBar from "../components/SearchBar";
 import ContactList from "../components/ContactsList";
+import { useState } from "react";
 
 const avatar = require('../assets/new.png');
 
 const NewChatScreen = () => {
+    const [modalVisible, setModalVisible] = useState(false)
 
     return(
         <View style={styles.mainContainer}>
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => {
+                    setModalVisible(!modalVisible);
+                }}>
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <Text style={styles.modalText}>Add new contact</Text>
+                        <SearchBar />
+                        <Pressable
+                            style={styles.button}
+                            onPress={() => setModalVisible(!modalVisible)}>
+                            <Text style={styles.buttonStyle}>Close</Text>
+                        </Pressable>
+                    </View>
+                </View>
+            </Modal>
             <SearchBar />
-            <Pressable style={styles.newContact}>
+            <Pressable 
+                onPress={() => {
+                    setModalVisible(true);
+                }}
+                style={styles.newContact}>
                 <Image style={styles.icon} source={avatar}/>
                 <Text style={styles.newContactText}>New Contact</Text>
             </Pressable>
@@ -45,7 +70,46 @@ const styles = StyleSheet.create({
         fontSize: 14,
         color: '#A8A5FF',
         paddingLeft: '2%',
-    }
+    },
+
+    centeredView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+      modalView: {
+        margin: 20,
+        backgroundColor: 'white',
+        borderRadius: 20,
+        padding: 35,
+        shadowColor: '#000',
+        shadowOffset: {
+          width: 0,
+          height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+      },
+      button: {
+        alignSelf: 'flex-end',
+        width: 120,
+        backgroundColor: '#A8A5FF',
+        borderRadius: 20,
+        marginTop: 15,
+        paddingVertical: 10,
+        elevation: 2
+      },
+      buttonStyle: {
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center',
+      },
+      modalText: {
+        marginBottom: 15,
+        paddingLeft: '2%',
+        textAlign: 'left',
+      },
 });
 
 export default NewChatScreen;
