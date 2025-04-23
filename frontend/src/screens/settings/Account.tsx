fe14a041753abf3846a51e6d929eda1179206991
mkytomies <moonakytomies@gmail.com>
2025-04-23 14:01:46 +0300

    Screens for different settings

---
+++ frontend/src/screens/settings/Account.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const AccountSettings = () => {
+    return(
+        <View>
+            <Text>Account Settings</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default AccountSettings;
---
+++ frontend/src/screens/settings/Privacy.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const PrivacySettings = () => {
+    return(
+        <View>
+            <Text>Privacy Settings</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default PrivacySettings;
