1d72a0aba766ca9c0bf9db2ef9de1cb3cd5ea497
mkytomies <moonakytomies@gmail.com>
2026-04-15 20:08:57 +0300

    Add setting for receiving notifications

--- frontend/src/screens/settings/Notifications.tsx
+++ frontend/src/screens/settings/Notifications.tsx
@@ -1,15 +1,33 @@
 import { StyleSheet, Text, View } from "react-native";
+import SwitchSetting from "../../components/SwitchSetting";
+import { useState } from "react";
 
 const NotificationSettings = () => {
+    const [getNotifications, setGetNotifications] = useState(true)
+    const handleNotificationSetting = () => {
+        setGetNotifications(!getNotifications);
+        console.log(getNotifications);
+        return getNotifications;
+    }
+
     return(
         <View>
-            <Text>Notification Settings</Text>
+            <Text style={styles.categoryText}>Notification Settings</Text>
+            <SwitchSetting 
+                initialState={true} 
+                settingText="Get notifications" 
+                onHandlePressed={handleNotificationSetting} 
+            />
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-
+    categoryText: {
+        color: '#A8A5FF',
+        fontSize: 14,
+        paddingTop: '10%',
+    },
 });
 
 export default NotificationSettings;
